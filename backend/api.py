a86ee99a0f5dded48c1fa8b705310cdbb6e86afa
Ryan Conwell <ryanconwell05@gmail.com>
2018-10-15 23:42:02 -0400

    IT WORKS. I DON'T KNOW WHAT I DID BUT IT WORKS.
    Also, haven't been committing as I go, so this is all together in
    one big blob. But screw it. It works. Somehow, all this illegible
    spaghetti code works. The email stuff is held on with duct tape. I
    have no idea why that's working. At some point I also apparently
    gave up on modularity. It's a rough prototype, it doesn't matter.
    What matters is that it works.
    
    If a Kenco employee is reading this, I am so so sorry. It's
    entirely possible that your boss is forcing you to use my code. If
    that's the case, you probably think I'm a moron. I am, but that's
    not the point. I was rushed, I took sloppy shortcuts. It works, but
    as a prototype. A very very rough prototype. Really, I'm normally
    better than this. Trust me. Anyways, this is going on too long,
    I'm mildly sleep-deprived, I have to present this tomorrow, and
    I still have more work to do.
    
    Ryan, signing off.

---
+++ backend/api.py
@@ -0,0 +1,30 @@
+from flask import (
+    Blueprint, request, current_app
+)
+from flask_mail import Mail, Message
+mail = Mail()
+
+from backend.db import get_db
+
+bp = Blueprint('api',__name__, url_prefix='/api')
+
+
+@bp.route('/restock', methods=('GET', 'POST'))
+def restock():
+    if request.method == 'POST':
+        device_id = request.form['device_id']
+
+        device = get_db().execute(
+            'SELECT * FROM device WHERE id = ?', (device_id,)
+        ).fetchone()
+
+        get_db().execute(
+            'INSERT INTO request (device_id) VALUES (?)', (device_id,)
+        )
+
+        msg = Message("New Restock Request!")
+        msg.recipients = [current_app.config['NOTIFICATION_RECIPIENT']]
+        msg.body = 'A new restock request was received.\n\n\nProduct: {}\nLocation: {}\n\n\nThis message was sent automatically by a prototype "smart button"'.format(device['product'], device['location'])
+        mail.send(msg)
+        # print('A new restock request was received.\n\n\nProduct: {}\nLocation: {}\n\n\nThis message was sent automatically by a prototype "Smart Button"'.format(device['product'], device['location']))
+        return "All good!"
